0bb0d825d92db1e8667821cdd69f4c55b199728c
Tone Test <92502536+Tone-Test@users.noreply.github.com>
2023-07-25 15:03:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -19,13 +19,16 @@ async def on_ready():
 async def on_message(msg: ds.Message):
     q = None
     if msg.reference and isinstance(msg.reference.resolved, ds.Message) and msg.reference.resolved.author.id == bot.user.id:
-        if msg.reference.resolved.reference.resolved and msg.reference.resolved.reference.resolved.author.id == msg.author.id: q = msg.content
-        else: await msg.channel.send(embed=ErrorEmbed('Это не ваш чат'), reference=msg)
+        if msg.reference.resolved.reference.resolved and msg.reference.resolved.reference.resolved.author.id == msg.author.id: 
+            q = msg.content
+        else: 
+            await msg.channel.send(embed=ErrorEmbed('This is not your chat'), reference=msg)
     if msg.content.startswith(f'<@{bot.user.id}>'):
         q = ' '.join(msg.content.split(' ')[1:])
-    if q == None: return
+    if q == None: 
+        return
     if q == '':
-        await msg.channel.send(embed=InfoEmbed('О FreeGPT', INFO_TEXT), reference=msg)
+        await msg.channel.send(embed=InfoEmbed('About FreeGPT', INFO_TEXT), reference=msg)
         return
     async with msg.channel.typing():
         try:
@@ -39,25 +42,28 @@ async def on_message(msg: ds.Message):
             udata['chat'] = chat
             savejson(str(msg.author.id), udata)
         except Exception as e:
-            await msg.channel.send(embed=ErrorEmbed('Ответ не получен\n'+str(e)), reference=msg)
+            await msg.channel.send(embed=ErrorEmbed('Response not received\n'+str(e)), reference=msg)
             return
     emb = MessageEmbed(q, res)
     emb.set_footer(text=model)
     await msg.channel.send(embed=emb, reference=msg)
 
-@bot.slash_command(descriprion='Сменить модель')
+@bot.slash_command(description='Change model')
 async def model(ctx: ctxt, model: ds.Option(str, choices=MODELS)):
     udata = {'chat': []}
     if os.path.exists(STORAGE + str(ctx.author.id)):
         udata = readjson(str(ctx.author.id))
     udata['model'] = model
     savejson(str(ctx.author.id), udata)
-    await ctx.respond(embed=InfoEmbed('Успешно', f'Модель изменена на {model}'), ephemeral=True)
+    await ctx.respond(embed=InfoEmbed('Successful', f'Model changed to {model}'), ephemeral=True)
 
-@bot.slash_command(description='Сбросить диалог')
+@bot.slash_command(description='Reset dialogue')
 async def reset(ctx: ctxt):
-    if os.path.exists(STORAGE + str(ctx.author.id)): chatlen = len(readjson(str(ctx.author.id))['chat']); os.remove(STORAGE + str(ctx.author.id))
-    else: chatlen = 0
-    await ctx.respond(embed=InfoEmbed('Успешно', f'Диалог из {chatlen} сообщений сброшен'), ephemeral=True)
+    if os.path.exists(STORAGE + str(ctx.author.id)): 
+        chatlen = len(readjson(str(ctx.author.id))['chat']); 
+        os.remove(STORAGE + str(ctx.author.id))
+    else: 
+        chatlen = 0
+    await ctx.respond(embed=InfoEmbed('Successful', f'Dialogue containing {chatlen} messages has been reset'), ephemeral=True)
 
 bot.run(os.getenv('TOKEN'))
